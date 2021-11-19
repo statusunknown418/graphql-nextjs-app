14833a8538fae6e6d83c1f414be5b6163fc43842
Alvaro_Aquije <alvarodevcode@outlook.com>
2021-11-19 18:43:56 -0500

    style(style): :lipstick: added tailwind styling

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      // * Config for the custom font
+      fontFamily: {
+        inter: 'Inter',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
